587b2dbd0275f778d1be99aac431555092f228af
huangzeng <huangzeng2016@126.com>
2020-11-07 23:09:06 -0800

    feat Finish static pages

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,10 @@
 Rails.application.routes.draw do
-  resources :microposts
-  resources :users
+  get 'static_pages/home'
+
+  get 'static_pages/help'
+
+  get 'static_pages/about'
+
 #  root 'application#hello'
-  root 'users#index'
+  root 'static_pages#home'
 end
