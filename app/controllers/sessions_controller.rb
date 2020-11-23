ed64fe4ece6f06f8f5c740218c69effeb0d18e0b
huangzeng <huangzeng2016@126.com>
2020-11-23 05:06:40 -0800

    base Log in

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,21 @@
+class SessionsController < ApplicationController
+  def new
+  end
+
+  def create
+    if params[:session][:email]
+      user = User.find_by(email: params[:session][:email].downcase)
+    end
+    if user && user.authenticate(params[:session][:password])
+      log_in user
+      redirect_to user
+    else
+      flash.now[:danger] = 'Invalid email/password combination'
+      render 'new'
+    end
+  end
+
+  def destory
+    
+  end
+end
